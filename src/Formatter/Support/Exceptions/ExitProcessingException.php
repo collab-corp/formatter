4eead570d769fe2421e790c86b6c501db62ce0da
Sergio Compean <scompean24@gmail.com>
2022-03-20 21:35:01 -0400

    clear ignore cache

--- src/Formatter/Support/Exceptions/ExitProcessingException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CollabCorp\Formatter\Support\Exceptions;
-
-use Exception;
-
-class ExitProcessingException extends Exception
-{
-}
--- tests/TestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CollabCorp\Formatter\Tests;
-
-use PHPUnit\Framework\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase
-{
-}
